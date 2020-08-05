440bf828438ff792f692cf96c58e47b9a6b5ecc5
Admin <admin@Admins-MacBook-Pro.local>
2020-08-05 15:34:31 +0700

    fix

--- MyPlayground2.playground/Contents.swift
+++ MyPlayground2.playground/Contents.swift
@@ -1,14 +1,23 @@
+//ý tưởng
+//1. lưu string vào mảng, sắp xếp theo tứ tự giảm dần tần số xuất hiện của kí tự
+//2. chọn từng phần tử một vào output sao cho không trùng vs kí tự cuối cùng của output
+//3. xoá phần tử vừa chọn, sắp xếp lại mảng, lặp lại bước 2
+
 func abc(_ str :String) -> String{
-    var arr = Array(str)
+    let arr = Array(str)
     var arrResult :Array<Character> = []
+    
+    var arrSorted = sort(arr) //sap xep theo thu tu tan so xuat hien cua ki tu
+    
     while arrResult.count < str.count {
-        for (index,char) in arr.enumerated() {
-            if arr.allSatisfy({$0 == arrResult.last}) {
+        for (index,char) in arrSorted.enumerated() {
+            if arrSorted.allSatisfy({$0 == arrResult.last}) {
                 return ""
             }
             if char != arrResult.last {
                 arrResult.append(char)
-                arr.remove(at: index)
+                arrSorted.remove(at: index)
+                arrSorted = sort(arrSorted)
                 break
             }
         }
@@ -16,8 +25,14 @@ func abc(_ str :String) -> String{
     return String(arrResult)
 }
 
+func sort(_ arr :Array<Character>) -> Array<Character> {
+    let arrSorted = arr.sorted { f, s in
+        arr.filter { $0 == f }.count > arr.filter { $0 == s }.count
+    }
+    return arrSorted
+}
 
-abc("aaaaaabbbcc")
+abc("abccaabcaaaab")
 
 
 
